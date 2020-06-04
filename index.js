2812117dcfe29168fa847018a3a16043684c473d
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-06-04 21:28:03 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const axios = require("axios");
-
-const urls = [
-    "https://aurora-discord.glitch.me/ping",
-    "http://ecobot-inex.glitch.me",
-    "http://pikabot-discord.glitch.me",
-    "http://giveawayboat-discord.glitch.me",
-    "http://snowboats.glitch.me"
-];
+const urls = require("./url.js");
 
 const uptimerobo = async () => {
     setInterval(() => {
