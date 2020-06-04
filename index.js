3710139fa2a60686b9fee3a467615d77855f495c
ZYROUGE <me@zyrouge.gq>
2020-06-04 21:05:18 +0530

    Added INSEX

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const axios = require("axios");
 
+const urls = [
+    "https://aurora-discord.glitch.me/ping",
+    "http://ecobot-inex.glitch.me",
+    "http://pikabot-discord.glitch.me",
+    "http://giveawayboat-discord.glitch.me",
+    "http://snowboats.glitch.me"
+];
+
 const uptimerobo = async () => {
     setInterval(() => {
-        /* Aurora Boat */
-        axios.get("https://auroradiscordbot.glitch.me/ping").then(() => console.log("Pong at " + Date.now()));
+        urls.forEach(url => {
+            axios.get(url).then(() => console.log("Pong at " + Date.now())).catch(() => {});
+        });
     }, 60 * 1000);
 };
 
